e3dca3596305d4e7079e8e42a1d0c51e33dd23a9
Fontom1 <105953388+FontomAss@users.noreply.github.com>
2024-03-30 20:42:53 +0700

    Добавьте файлы проекта.

---
+++ fibonachchi/fibonachchi.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int F(int n)
+{
+    if (n == 0)return 0;
+    else if (n == 1)return 1;
+    else if (n > 1) return  F(n - 1) + F(n - 2);
+}
+
+int main()
+{
+    int a;
+    std::cin >> a;
+    std::cout << F(a);
+}
